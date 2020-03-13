70a5eac3381c7244b38b9822877b932da1409518
Daniel Nunez <JorgeDaniel.NunezGarcia@us.bosch.com>
2020-03-13 14:19:38 -0600

    pre-front-end first steps

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-const request = require('request')
+//const request = require('request')
+const forecast = require('./utils/forecast.js')
+const geocode = require('./utils/geocode.js')
 
-const url = 'https://api.darksky.net/forecast/acbcb4f32f32f32836fc843489a8ff92/37.8267,-122.4233'
 //edit info using information from api, edit at the end ?units=us
 //const url = 'https://api.darksky.net/forecast/acbcb4f32f32f32836fc843489a8ff92/37.8267,-122.4233?units=us'
 
@@ -10,7 +11,7 @@ request({ url: url }, (error, response) => {
     const data = JSON.parse(response.body)
     console.log(data)
 })
-*/
+
 
 //Request tiene una propiedad que se puede hacer parsing de json al definir la funcion
 
@@ -20,10 +21,11 @@ request({ url: url, json: true }, (error, response) => {
     //console.log('It is currently ' + response.body.currently.temperature + ' degrees')
     console.log(response.body.daily.data[0].summary + ' degrees')
 })
-
+*/
 
 //Challenge
 
+/*
 url_geo = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiZGFuaWVsb3dza2kiLCJhIjoiY2s3cDd2MXVpMGh2ajNocDBna2piMTJ1OSJ9.WfcA6j_mgoHNw3aYWbuBxg&limit=1'
 
 request({url: url_geo, json: true}, (error, response) => {
@@ -34,5 +36,52 @@ request({url: url_geo, json: true}, (error, response) => {
     } else {
         const coordinates = response.body.features[0].geometry.coordinates
         console.log(coordinates)
+        //Utilizar funciones dentro de un archivo Utils
+        forecast(44.1545, -75.7088, (error, data) => {
+            console.log('Error', error)
+            console.log('Data', data)
+        })
     }
 })
+
+*/
+
+const address = process.argv[2]
+
+if (!address) {
+    console.log('Please provide an address')
+} else {
+    /*
+    geocode(address, (error, data) => {
+        if (error) {
+            return console.log(error)
+        }
+
+        forecast(data.latitude, data.longitude, (error, forecastData) => {
+            if (error) {
+                return console.log(error)
+            }
+
+            console.log(data.location)
+            console.log(forecastData)
+        })
+    })
+    */
+
+    // Destructuring with ES6
+    geocode(address, (error, {latitude, longitude, location}) => {
+        if (error) {
+            return console.log(error)
+        }
+
+        forecast(latitude, longitude, (error, forecastData) => {
+            if (error) {
+                return console.log(error)
+            }
+
+            console.log(location)
+            console.log(forecastData)
+        })
+    })
+}
+
